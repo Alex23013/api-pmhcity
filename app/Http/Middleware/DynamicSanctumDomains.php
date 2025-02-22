aa6131223c1538fafacd993e73f20197d10b0c47
Maria Cornejo <mcornejo@adaction.com>
2025-02-22 12:33:39 -0500

    Add: logging

--- app/Http/Middleware/DynamicSanctumDomains.php
+++ app/Http/Middleware/DynamicSanctumDomains.php
@@ -5,6 +5,7 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\Log;
 
 class DynamicSanctumDomains
 {
@@ -12,6 +13,11 @@ class DynamicSanctumDomains
     {
         $origin = $request->header('Origin');
         $allowedDomains = Config::get('sanctum.stateful', []);
+
+        Log::info('DynamicSanctumDomains Middleware Called', [
+            'origin' => $origin,
+            'before' => $allowedDomains,
+        ]);
 
         // If the origin is valid and not already in the list, add it
         if ($origin && !in_array($origin, $allowedDomains, true)) {
