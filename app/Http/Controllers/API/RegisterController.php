f3e4583977cd6591c9943cf9ffa2cc20857080fd
Maria Cornejo <mcornejo@adaction.com>
2025-02-22 12:58:15 -0500

    Add: Loggs

--- app/Http/Controllers/API/RegisterController.php
+++ app/Http/Controllers/API/RegisterController.php
@@ -51,7 +51,7 @@ class RegisterController extends BaseController
             $user = Auth::user(); 
             $success['token'] =  $user->createToken('MyApp')->plainTextToken; 
             $success['name'] =  $user->name;
-            Log::info('Test logs');
+            dd('Test logs');
    
             return $this->sendResponse($success, 'User login successfully.');
         } 
