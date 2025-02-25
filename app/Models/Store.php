027c608d81270366f5b4f0129190f7c5bac053da
Maria Cornejo <mcornejo@adaction.com>
2025-02-25 00:21:50 -0500

    feat: create models and migrations

---
+++ app/Models/Store.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\User;
+
+class Store extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'name',
+        'siret',
+        'is_verified'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class,'user_id');
+    }
+}
