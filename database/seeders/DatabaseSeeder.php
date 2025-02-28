e119738010b10e3ce40d2d1790f6a41edc9c4625
Maria Cornejo <mcornejo@adaction.com>
2025-02-28 02:39:43 -0500

    feat: update product controller and other seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,5 +18,6 @@ class DatabaseSeeder extends Seeder
         $this->call(CategorySeeder::class);
         $this->call(ReservationStatusSeeder::class);
         $this->call(DataProductSeeder::class);
+        $this->call(ColorSeeder::class);
     }
 }
