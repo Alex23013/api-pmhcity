e3422f5cdeed7658ba50228d8f8cf6a400f52d4e
Maria Cornejo <mcornejo@adaction.com>
2025-03-24 11:29:49 -0500

    fix: change models to allow change name

--- app/Models/Color.php
+++ app/Models/Color.php
@@ -3,8 +3,17 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Product;
 
 class Color extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = ['name', 'hex_code'];
+
+    public function products()
+    {
+        return $this->hasMany(Product::class);
+    }
 }
